55716bacc914ffc00aaa9fa926ffd4549313a70f
Phil Wang <lucidrains@gmail.com>
2021-12-05 09:44:55 -0800

    setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'x-clip',
+  packages = find_packages(exclude=[]),
+  version = '0.0.1',
+  license='MIT',
+  description = 'X-CLIP',
+  author = 'Phil Wang',
+  author_email = 'lucidrains@gmail.com',
+  url = 'https://github.com/lucidrains/x-clip',
+  keywords = [
+    'artificial intelligence',
+    'deep learning',
+    'contrastive learning',
+    'CLIP',
+  ],
+  install_requires=[
+    'einops>=0.3',
+    'torch>=1.6'
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
