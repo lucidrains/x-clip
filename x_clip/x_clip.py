fix FILIP variable text length masking, add ability to compose InfoLOOB, cite

x_clip/x_clip.py
@@ -12,9 +12,17 @@ def exists(val):
 def default(val, d):
     return val if exists(val) else d
 
-def masked_mean(t, mask, dim = 1):
-    t = t.masked_fill(~mask[:, :, None], 0.)
-    return t.sum(dim = 1) / mask.sum(dim = 1, keepdim = True)
+def max_neg_value(dtype):
+    return -torch.finfo(dtype).max
+
+def masked_mean(t, mask, dim = 1, eps = 1e-6):
+    t = t.masked_fill(~mask, 0.)
+    numer = t.sum(dim = dim)
+    denom = mask.sum(dim = dim).clamp(min = eps)
+    return numer / denom
+
+def log(t, eps = 1e-20):
+    return torch.log(t + eps)
 
 def l2norm(t):
     return F.normalize(t, dim = -1, p = 2)
@@ -200,7 +208,9 @@ class CLIP(nn.Module):
         visual_image_size = 256,
         visual_patch_size = 32,
         channels = 3,
-        use_all_token_embeds = False
+        use_all_token_embeds = False,
+        decoupled_contrastive_learning = False,
+        extra_latent_projection = False
     ):
         super().__init__()
         self.text_transformer = TextTransformer(
@@ -228,12 +238,22 @@ class CLIP(nn.Module):
         # from https://arxiv.org/abs/2111.07783 (FILIP paper)
         self.use_all_token_embeds = use_all_token_embeds
 
+        # proposed in https://arxiv.org/abs/2110.06848 (DCL) and https://arxiv.org/abs/2110.11316 (CLOOB)
+        self.decoupled_contrastive_learning = decoupled_contrastive_learning
+
+        # proposed in https://arxiv.org/abs/2110.11316 (CLOOB)
+        self.extra_latent_projection = extra_latent_projection
+
+        self.to_text_latent_extra = nn.Linear(dim_text, dim_latent, bias = False)
+        self.to_visual_latent_extra = nn.Linear(dim_image, dim_latent, bias = False)
+
     def forward(
         self,
         text,
         image,
         text_mask = None,
-        return_loss = False
+        return_loss = False,
+        text_to_image = True   # in the case the extra projection is turned on, would return different similarity values depending on modality directionality
     ):
         b, device = text.shape[0], text.device
 
@@ -255,27 +275,79 @@ class CLIP(nn.Module):
         image_latents = self.to_visual_latent(image_embeds)
         text_latents, image_latents = map(l2norm, (text_latents, image_latents))
 
+        # calculate another set of latents for image to text (vs text to image)
+        # proposed by CLOOB
+
+        text_latents_extra, image_latents_extra = text_latents, image_latents
+        if self.extra_latent_projection:
+            text_latents_extra = self.to_text_latent_extra(text_embeds)
+            image_latents_extra = self.to_visual_latent_extra(image_embeds)
+            text_latents_extra, image_latents_extra = map(l2norm, (text_latents_extra, image_latents_extra))
+
         # get temperature
 
         temp = self.temperature.exp()
 
+        # early return, if needed
+
         if not return_loss and self.use_all_token_embeds:
-            return einsum('b t d, b i d -> b t i', text_latents, image_latents) * temp
+            einsum_args = (text_latents_extra, image_latents_extra) if self.extra_latent_projection and not text_to_image else (text_latents, image_latents)
+            return einsum('b t d, b i d -> b t i', *einsum_args) * temp
 
         if not return_loss and not self.use_all_token_embeds:
-            return einsum('b d, b d -> b', text_latents, image_latents) * temp
+            einsum_args = (text_latents_extra, image_latents_extra) if self.extra_latent_projection and not text_to_image else (text_latents, image_latents)
+            return einsum('b d, b d -> b', *einsum_args) * temp
 
         # contrastive loss
 
         if self.use_all_token_embeds:
             # fine-grained CLIP logic
-            sim = einsum('x t d, y i d -> x y t i', text_latents, image_latents) * temp
-            text_to_image = reduce(reduce(sim, 'bt bi t i -> bt bi t', 'max'), 'bt bi t -> bt bi', 'mean')
-            image_to_text = reduce(reduce(sim, 'bt bi t i -> bt bi i', 'max'), 'bt bi i -> bt bi', 'mean')
-        else:
-            sim = einsum('t d, i d -> t i', text_latents, image_latents) * temp
-            text_to_image, image_to_text = sim, sim.t()
+            sim_text_to_image = einsum('x t d, y i d -> x y t i', text_latents, image_latents) * temp
 
-        labels = torch.arange(b, device = sim.device)
-        loss = (F.cross_entropy(text_to_image, labels) + F.cross_entropy(image_to_text, labels)) / 2
+            sim_image_to_text = sim_text_to_image
+            if self.extra_latent_projection:
+                sim_image_to_text = einsum('x t d, y i d -> x y t i', text_latents_extra, image_latents_extra) * temp
+
+            if exists(text_mask):
+                text_to_image = reduce(sim_text_to_image, 'bt bi t i -> bt bi t', 'max')
+                text_to_image_mask = rearrange(text_mask, 'bt t -> bt 1 t')
+                text_to_image = masked_mean(text_to_image, text_to_image_mask, dim = -1)
+
+                image_to_text_mask = rearrange(text_mask, 'bt t -> bt 1 t 1')
+                masked_sim = sim_image_to_text.masked_fill(~image_to_text_mask, max_neg_value(sim_image_to_text.dtype))
+                image_to_text = reduce(reduce(masked_sim, 'bt bi t i -> bt bi i', 'max'), 'bt bi i -> bt bi', 'mean')
+            else:
+                text_to_image = reduce(reduce(sim_text_to_image, 'bt bi t i -> bt bi t', 'max'), 'bt bi t -> bt bi', 'mean')
+                image_to_text = reduce(reduce(sim_image_to_text, 'bt bi t i -> bt bi i', 'max'), 'bt bi i -> bt bi', 'mean')
+        else:
+            text_to_image = einsum('t d, i d -> t i', text_latents, image_latents) * temp
+            image_to_text = text_to_image
+
+            if self.extra_latent_projection:
+                image_to_text = einsum('t d, i d -> t i', text_latents_extra, image_latents_extra) * temp
+
+        # calculate loss
+
+        # exponentiate
+
+        text_to_image_exp, image_to_text_exp = map(torch.exp, (text_to_image, image_to_text))
+
+        # numerators
+
+        text_to_image_pos, image_to_text_pos = map(torch.diag, (text_to_image_exp, image_to_text_exp))
+
+        # denominator
+
+        if self.decoupled_contrastive_learning:
+            pos_mask = torch.eye(b, device = device, dtype = torch.bool)
+            text_to_image_exp, image_to_text_exp = map(lambda t: t.masked_fill(pos_mask, 0.), (text_to_image_exp, image_to_text_exp))
+
+        text_to_image_denom, image_to_text_denom = map(lambda t: t.sum(dim = -1), (text_to_image_exp, image_to_text_exp))
+
+        # loss
+
+        text_to_image_loss = -log(text_to_image_pos / text_to_image_denom).mean()
+        image_to_text_loss = -log(image_to_text_pos / image_to_text_denom).mean()
+
+        loss = (text_to_image_loss + image_to_text_loss) / 2
         return loss